a183787dd4dbc82caecd2fb27bafd505a4daecdf
CarolineMatte <caroline.m2007@aluno.ifsc.edu.br>
2026-05-11 17:13:02 -0300

    commit
    commit

---
+++ php/blog/admin/index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+include_once './database/db.class.php';
+
+
+//instanciar um objeto da classe DB
+$conn = new db("aluno");
+
+$dados = [
+    'nome' =>"Caroline Matte",
+    'telefone' => "49 8504-0369",
+    'email' => 'caroline.m2007@aluno.ifsc.edu.br',
+
+];
+
+$conn->store($dados);
+echo "Inserido com Sucesso!";
